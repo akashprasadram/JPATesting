a3fc066f491a46f422d2ab8ffbf9fc5ec9468020
Akash Prasad Ram <60048414+akashprasadram@users.noreply.github.com>
2023-06-11 17:52:04 +0530

    Revert "simplified controller methods."

--- src/main/java/com/spring/jpaLearning/controller/StudentController.java
+++ src/main/java/com/spring/jpaLearning/controller/StudentController.java
@@ -1,9 +1,9 @@
 package com.spring.jpaLearning.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.spring.jpaLearning.dto.StudentDtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,28 +19,54 @@ import com.spring.jpaLearning.repo.StudentRepo;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-
+	
 	@Autowired
 	private StudentRepo studentRepo;
-
-	@Autowired
-	private StudentDtoMapper studentDtoMapper;
-
 	
 	@PostMapping("/")
-	public StudentDTO addStudent(@RequestBody Student student) {
-		return studentDtoMapper.apply(studentRepo.save(student));
+	public StudentDTO addStudent(@RequestBody Student s)
+	{
+		System.out.println("Student="+s);
+		Student student1=studentRepo.save(s);
+		
+		StudentDTO student=new StudentDTO();
+		student.setId(student1.getId());
+		student.setName(student1.getName());
+		student.setSubjects(student1.getSubjects());
+		student.setTeacher(student1.getTeacher());
+		
+		return student;
 	}
 	
 	@GetMapping("/{id}")
-	public StudentDTO getStudentById(@PathVariable("id") int id) {
-		return studentDtoMapper.apply(studentRepo.findById(id).orElseGet(Student::new));
+	public StudentDTO getStudentById(@PathVariable("id") int id)
+	{
+		Student student1=studentRepo.findById(id).get();
+		
+		StudentDTO student=new StudentDTO();
+		student.setId(student1.getId());
+		student.setName(student1.getName());
+		student.setSubjects(student1.getSubjects());
+		student.setTeacher(student1.getTeacher());
+		System.out.println(student);
+		return student;
 	}
 	
 	@GetMapping("/")
 	public List<StudentDTO> getAllStudent()
 	{
-		return studentRepo.findAll().stream().map(studentDtoMapper).toList();
+		List<Student> students1=studentRepo.findAll();
+		List<StudentDTO> students=new ArrayList<>();
+		for(Student s:students1)
+		{
+			StudentDTO s1=new StudentDTO();
+			s1.setId(s.getId());
+			s1.setName(s.getName());
+			s1.setSubjects(s.getSubjects());
+			s1.setTeacher(s.getTeacher());
+			students.add(s1);
+		}
+		return students;
 	}
 
 }
--- src/main/java/com/spring/jpaLearning/controller/TeacherController.java
+++ src/main/java/com/spring/jpaLearning/controller/TeacherController.java
@@ -3,7 +3,6 @@ package com.spring.jpaLearning.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.spring.jpaLearning.dto.TeacherDtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,25 +18,49 @@ import com.spring.jpaLearning.repo.TeacherRepo;
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
-
 	@Autowired
 	private TeacherRepo teacherRepo;
-
-	@Autowired
-	private TeacherDtoMapper teacherDtoMapper;
-
+	
 	@PostMapping("/")
-	public TeacherDTO addTeacher(@ModelAttribute("Teacher") Teacher teacher) {
-		return teacherDtoMapper.apply(teacherRepo.save(teacher));
+	public TeacherDTO addTeacher(@ModelAttribute("Teacher") Teacher t)
+	{
+		System.out.println("Teacher="+t);
+		Teacher teacher1=teacherRepo.save(t);
+		
+		TeacherDTO teacher=new TeacherDTO();
+		teacher.setId(teacher1.getId());
+		teacher.setName(teacher1.getName());
+		teacher.setStudents(teacher1.getStudents());
+		
+		return teacher;
 	}
 	
 	@GetMapping("/{id}")
-	public TeacherDTO getTeacherById(@PathVariable("id") int id) {
-		return teacherDtoMapper.apply(teacherRepo.findById(id).orElseGet(Teacher::new));
+	public TeacherDTO getTeacherById(@PathVariable("id") int id)
+	{
+		Teacher teacher1=teacherRepo.findById(id).get();
+		
+		TeacherDTO teacher=new TeacherDTO();
+		teacher.setId(teacher1.getId());
+		teacher.setName(teacher1.getName());
+		teacher.setStudents(teacher1.getStudents());
+		System.out.println(teacher);
+		return teacher;
 	}
 	
 	@GetMapping("/")
-	public List<TeacherDTO> getAllTeacher() {
-		return teacherRepo.findAll().stream().map(teacherDtoMapper).toList();
+	public List<TeacherDTO> getAllTeacher()
+	{
+		List<Teacher> teachers1=teacherRepo.findAll();
+		List<TeacherDTO> teachers=new ArrayList<>();
+		for(Teacher t:teachers1)
+		{
+			TeacherDTO t1=new TeacherDTO();
+			t1.setId(t.getId());
+			t1.setName(t.getName());
+			t1.setStudents(t.getStudents());
+			teachers.add(t1);
+		}
+		return teachers;
 	}
 }
